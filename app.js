dd44ccdf3fd48cd9afb7e18c690cdd68de151818
Sofia <burovasofia@list.ru>
2022-12-12 07:58:23 +0300

    commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#ffc0cb', '#30d5c8', '#ffaacc', '#ff2483', '#2af7ed', '#8b00ff', '#5200cc', '#9400d3', '#42aaff', '#fff538']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+   const index = Math.floor(Math.random() * colors.length)
+   return  colors[index]
+}
